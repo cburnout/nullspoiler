a4c8b086782f84ec9ba9d0362b4047cd0c81f4e9
Charles Burnell <c.burnell@gmail.com>
2017-09-03 12:18:07 -0600

    working getting spoilers to the server

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-http.createServer(function (req, res) {
-    fs.readFile('createlog.html', function(err, data) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        res.end();
-    });
-    var queryData = url.parse(req.url, true).query;
-    console.log(queryData);
-}).listen(8080);
+process.setMaxListeners(0);
+var http = require('http'),
+    fs = require('fs'),
+    // NEVER use a Sync function except at start-up!
+    index = fs.readFileSync(__dirname + '/index.html');
+
+// Send index.html to all requests
+var app = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(index);
+});
+
+// Socket.io server listens to our app
+var io = require('socket.io').listen(app);
+
+// Emit welcome message on connection
+io.on('connection', function(socket) {
+    // Use socket to communicate with this particular client only, sending it it's own id
+    socket.emit('welcome', { message: 'Welcome!', id: socket.id });
+
+    socket.on('i am client', console.log);
+	socket.on('rec-final', console.log);
+	socket.on('submit spoiler', function(data) {
+	console.log(data);	
+});
+});
+
+
+app.listen(3000);
