04bee4b57a322eac0efafffa7f24b3852634dc7b
Charles Burnell <c.burnell@gmail.com>
2017-09-03 13:07:02 -0600

    working on show

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 var http = require('http'),
 	fs = require('fs'),
-	// NEVER use a Sync function except at start-up!
-	index = fs.readFileSync(__dirname + '/index.html')
-	showlog = fs.readFileSync(__dirname + '/show.html')
-	createlog = fs.readFileSync(__dirname + '/create.html');
+	url = require('url');
+// NEVER use a Sync function except at start-up!
+index = fs.readFileSync(__dirname + '/index.html')
+showlog = fs.readFileSync(__dirname + '/show.html')
+createlog = fs.readFileSync(__dirname + '/create.html');
 
 // Send index.html to all requests
 var app = http.createServer(function(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end(index);
+
+	var pathname = url.parse(req.url).pathname;
+	switch(pathname){
+		case '/index.html':
+		case '/':
+			res.end(index);
+			break;
+		case '/show.html':
+			res.end(showlog)
+			break;
+		case '/create.html':
+			res.end(createlog)
+			break;
+	}
 });
 
 // Socket.io server listens to our app
@@ -27,6 +41,10 @@ io.on('connection', function(socket) {
 			if (err) throw err;
 			console.log('saved');
 		});	
+	});
+	socket.on('show log', function(data) {
+		var to_send = fs.readFile(__dirname + '/spoilerlogs/test.log');
+		socket.emit('return log', to_send);
 	});
 });
 
